644c740c980f35d9f99c4b656f3715da42c4e060
Yerbol Sultanbayev <sultanbayev@gmail.com>
2022-07-28 05:48:46 +0600

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function toReadable (number) {
-  
+    const onesAndTensReadable = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    const tensReadable = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+    if (number === 0) return 'zero';
+
+    let hundreds = Math.floor(number / 100);
+    let tensAndOnes = number - hundreds * 100;
+
+    hundreds = hundreds !== 0 ? onesAndTensReadable[hundreds] + ' hundred' : '';
+
+    if (tensAndOnes === 0) {
+        tensAndOnes = ''
+    } else if (tensAndOnes < 20) {
+        tensAndOnes = onesAndTensReadable[tensAndOnes];
+    } else {
+        let tens = Math.floor(tensAndOnes / 10);
+        let ones =  tensAndOnes - tens * 10;
+        tens = (tens !== 0) ? tensReadable[tens] : '';
+        ones = (ones !== 0) ? onesAndTensReadable[ones] : ''
+        tensAndOnes = `${tens}${ tens && ones ? ' ' : '' }${ones}`
+    }
+
+    return `${hundreds}${ hundreds && tensAndOnes ? ' ' : '' }${tensAndOnes}`;
 }
